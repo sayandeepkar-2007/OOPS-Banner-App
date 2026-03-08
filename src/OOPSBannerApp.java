78d59f0010e4026078fb72944584ef26addd93ed
sayandeepkar-2007 <sayandeepkar28@gmail.com>
2026-03-08 14:43:43 +0530

    UC8 - Banner using HashMap collection

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,48 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
-
-    class BannerPrinter {
-
-        String[] O = {
-            " *** ",
-            "*   *",
-            "*   *",
-            "*   *",
-            " *** "
-        };
-
-        String[] P = {
-            "**** ",
-            "*   *",
-            "**** ",
-            "*    ",
-            "*    "
-        };
-
-        String[] S = {
-            " ****",
-            "*    ",
-            " *** ",
-            "    *",
-            "**** "
-        };
-
-        void printBanner() {
-
-            for (int i = 0; i < 5; i++) {
-                System.out.println(O[i] + "  " + O[i] + "  " + P[i] + "  " + S[i]);
-            }
-
-        }
-
-    }
 
     public static void main(String[] args) {
 
-        OOPSBannerApp app = new OOPSBannerApp();
-        BannerPrinter printer = app.new BannerPrinter();
+        Map<Character, String[]> bannerMap = new HashMap<>();
 
-        printer.printBanner();
+        bannerMap.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        bannerMap.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        bannerMap.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+
+        String word = "OOPS";
+
+        for (int i = 0; i < 5; i++) {
+
+            for (char c : word.toCharArray()) {
+                System.out.print(bannerMap.get(c)[i] + "  ");
+            }
+
+            System.out.println();
+        }
 
     }
-
 }
